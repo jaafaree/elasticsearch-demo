333aa7fad289a370836b40d9575aa7662cab7ef0
jaafaree <416259779@qq.com>
2019-06-17 00:15:11 +0800

    Add status exception handler and make some simple test case

--- DataSearchService/DataSearchCommon/src/main/java/com/jaafar/data/common/handler/GlobalExceptionHandler.java
+++ DataSearchService/DataSearchCommon/src/main/java/com/jaafar/data/common/handler/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
@@ -19,9 +20,10 @@ public class GlobalExceptionHandler {
     public static final String KEY_EXCEPTION_CAUSE   = "cause";
 
     @ExceptionHandler(Exception.class)
+    @SuppressWarnings({ "unchecked" })
     public ResponseEntity otherExceptionHandler(HttpServletResponse response, Exception ex){
         log.error("otherExceptionHandler catch ex: " + ex.getMessage(), ex);
-        return new ResponseEntity(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity(this.buildHeaders(ex), null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(BaseBusinessException.class)
@@ -31,7 +33,14 @@ public class GlobalExceptionHandler {
         return new ResponseEntity(this.buildHeaders(ex), null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    private Map<String, String> buildHeaders(BaseBusinessException ex) {
+    @ExceptionHandler(ResponseStatusException.class)
+    @SuppressWarnings({ "unchecked" })
+    public ResponseEntity responseStatusExceptionHandler(HttpServletResponse response, ResponseStatusException ex){
+        log.error("responseStatusExceptionHandler catch ex: " + ex.getMessage(), ex);
+        return new ResponseEntity(this.buildHeaders(ex), null, ex.getStatus());
+    }
+
+    private Map<String, String> buildHeaders(Exception ex) {
         Map<String, String> headers = new HashMap<>();
         headers.put(KEY_EXCEPTION_MESSAGE, ex.getMessage());
         return headers;
--- DataSearchService/DataSearchEndpoint/src/main/java/com/jaafar/data/search/controller/SearchController.java
+++ DataSearchService/DataSearchEndpoint/src/main/java/com/jaafar/data/search/controller/SearchController.java
@@ -1,9 +1,12 @@
 package com.jaafar.data.search.controller;
 
-import com.jaafar.data.common.exception.BaseBusinessException;
 import com.jaafar.data.common.VO.BaseResponse;
+import com.jaafar.data.common.exception.BaseBusinessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * Searching related endpoints
@@ -14,8 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SearchController {
 
-    @GetMapping("/test")
-    public BaseResponse test() {
-        throw new BaseBusinessException("test ex");
+    @GetMapping("/test/{type}")
+    public BaseResponse testExceptionHandler(@PathVariable String type) {
+        switch (type) {
+            case "base":
+                throw new BaseBusinessException("test ex");
+            case "status":
+                throw new ResponseStatusException(HttpStatus.NO_CONTENT, "test status ex");
+            default:
+                throw new RuntimeException("no type error");
+        }
     }
 }
