f802326bd2223f63be2e0ffbf4e8e46e85c0027b
jaafaree <416259779@qq.com>
2019-06-17 23:27:28 +0800

    Add seeder module

---
+++ DataSearchService/DataSearchSeeder/src/main/java/com/jaafar/data/seeder/DataSearchSeederApplication.java
@@ -0,0 +1,13 @@
+package com.jaafar.data.seeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSearchSeederApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSearchSeederApplication.class, args);
+    }
+
+}
